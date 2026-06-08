03149813ab1eab7b15d9dc69d65e56c7882440cd
spiderasasen <spiderasasen@gmail.com>
2026-06-08 18:57:29 -0400

    artical link is done

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import Header from "../components/Header.jsx"
+import "../styles/artical.css"
 
 function Home() {
     return (
         <>
+            {/*header code*/}
             <div>
                 <Header />
             </div>
+
+            {/*main code*/}
+            <main>
+                {/*artical link*/}
+                <div>
+                    <a className="vet-article-link"
+                       href={"https://www.elon.edu/u/news/2025/08/04/campus-alamance-celebrates-five-years-of-elon-students-in-the-community/"}
+                       target={"_blank"}>
+                        Read Semaj Clark's being featured in Elon News Article
+                    </a>
+                </div>
+            </main>
         </>
     )
 }
