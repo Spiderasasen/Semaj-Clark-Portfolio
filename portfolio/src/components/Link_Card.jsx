8e88cc18f5e2a60e69dcde97a73dd6687634def6
spiderasasen <spiderasasen@gmail.com>
2026-06-09 10:50:10 -0400

    got the style for the contacts

--- portfolio/src/components/Link_Card.jsx
+++ portfolio/src/components/Link_Card.jsx
@@ -1,21 +1,23 @@
+import "../styles/contact_card.css"
+
 function Link_Card({name, link}) {
     if (name !== "email" && name !== "phone"){
         return(
-            <a href={link} target={"_blank"}>
+            <a className={"link-card"} href={link} target={"_blank"}>
                 {name}
             </a>
         )
     }
     else if (name === "email") {
         return(
-            <a href={"mailto:" + link}>
+            <a className={"link-card"} href={"mailto:" + link}>
                 {name}
             </a>
         )
     }
     else if (name === "phone") {
         return(
-            <p>
+            <p className={"link-card"}>
                 {name}: {link}
             </p>
         )
